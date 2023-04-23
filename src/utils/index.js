108133edfa1856c4561d79a8b5674c0fca8348a0
Punnapob Jindakul <punnapob.jdk@gmail.com>
2023-04-23 18:26:30 +0700

    feat: add search&filter feature

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,28 +1,27 @@
-exports.timeToInt=(time)=>{
-    const timeSplit = time.split(":")
-    const timeInt=parseInt(timeSplit[0])*60+parseInt(timeSplit[1])
+exports.timeToInt = (time) => {
+    const timeSplit = time.split(":");
+    const timeInt = parseInt(timeSplit[0]) * 60 + parseInt(timeSplit[1]);
     return timeInt;
-}
-exports.isTimeBetween = (start, end, checkstart, checkend) => {
-
-    // Parse the input strings into Date objects
-    const startTime = this.timeToInt(start)
-    const endTime = this.timeToInt(end);
-    const checkStartTime =this.timeToInt(checkstart);
-    const checkEndTime =this.timeToInt(checkend);
-    if(startTime<=checkStartTime && endTime>=checkEndTime){
-        return true
-    }
-    return false
 };
-exports.isOverlap=(start, end, checkstart, checkend) => {
-
+exports.isTimeBetween = (start, end, checkstart, checkend) => {
     // Parse the input strings into Date objects
-    const startTime = this.timeToInt(start)
+    const startTime = this.timeToInt(start);
     const endTime = this.timeToInt(end);
-    const checkStartTime =this.timeToInt(checkstart);
-    const checkEndTime =this.timeToInt(checkend);
-    if(endTime<=checkStartTime)return false;
-    if(checkEndTime<=startTime)return false;
+    const checkStartTime = this.timeToInt(checkstart);
+    const checkEndTime = this.timeToInt(checkend);
+
+    if (startTime <= checkStartTime && endTime >= checkEndTime) {
+        return true;
+    }
+    return false;
+};
+exports.isOverlap = (start, end, checkstart, checkend) => {
+    // Parse the input strings into Date objects
+    const startTime = this.timeToInt(start);
+    const endTime = this.timeToInt(end);
+    const checkStartTime = this.timeToInt(checkstart);
+    const checkEndTime = this.timeToInt(checkend);
+    if (endTime <= checkStartTime) return false;
+    if (checkEndTime <= startTime) return false;
     return true;
 };
